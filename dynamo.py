349078d9373283e13047eda663710eab84391680
srubey <scrubey@pdx.edu>
2021-08-02 09:34:52 -0600

    Completed dynamo.py functions

---
+++ dynamo.py
@@ -0,0 +1,62 @@
+import boto3
+from datetime import datetime
+
+class dynamo():
+    def __init__(self):
+        self.database = boto3.resource('dynamodb', region_name = 'us-east-1')
+        self.table = self.database.Table('chatMessages')
+
+        #attempt to load the table from dynamodb
+        try:
+            self.table.load()
+        #if table not found, create one
+        except:
+            self.database.create_table(
+                TableName = 'chatMessages',
+                KeySchema = [
+                    {
+                        'AttributeName': 'timestamp',
+                        'KeyType': 'RANGE'
+                    },
+                    {
+                        'AttributeName': 'message',
+                        'KeyType': 'HASH'
+                    }
+                ],
+                AttributeDefinitions = [
+                    {
+                        'AttributeName': 'timestamp',
+                        'AttributeType': 'S'
+                    },
+                    {
+                        'AttributeName': 'message',
+                        'AttributeType': 'S'
+                    }
+                ],
+                ProvisionedThroughput = {
+                    "ReadCapacityUnits": 1,
+                    "WriteCapacityUnits": 1
+                }
+            )
+
+            def getMessages(self):
+                try:
+                    messages = self.table.scan()
+                except:
+                    return('Could not connect to database')
+
+                return([[m['timestamp'], m['message']] for m in messages['msgObj']])
+
+            def insertNewMessage(self, timestamp, message):
+                messageObj = {
+                    'timestamp': str(datetime.today()),
+                    'message': message
+                }
+
+                try:
+                    self.table.put_item(Item = messageObj)
+                except:
+                    return False
+
+                return True
+
