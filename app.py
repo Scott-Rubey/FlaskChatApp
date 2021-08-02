c7d68a73a77938d20558cf83f50fe9d65ba66bd9
srubey <scrubey@pdx.edu>
2021-08-02 13:53:08 -0600

    baseline

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
-from flask import Flask
-from flask import render_template
-from dynamo import dynamo
+from flask import Flask, render_template
+from Dynamo import dynamo
 
 app = Flask(__name__)
 dynamo = dynamo()
 
-@app.route('/')
+@app.route('/', methods = ['GET', 'POST'])
 def index():  # put application's code here
-    messages = [dict(timestamp = msg[0], message = msg[1]) for msg in dynamo.getMessages()]
+    messages = [dict(timestamp = msg[0], message = msg[1]) for msg in dynamo.select()]
     return render_template('index.html', messages = messages)
 
 if __name__ == '__main__':
