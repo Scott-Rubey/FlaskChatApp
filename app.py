3705883796b461e63226c256a4524f6790105bdf
srubey <scrubey@pdx.edu>
2021-08-02 10:00:46 -0600

    Added functionality for passing messages from dynamo to index.html

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask
 from flask import render_template
+from .dynamo import dynamo
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():  # put application's code here
-    return render_template('index.html')
-
+    messages = [dict(timestamp = msg[0], message = msg[1]) for msg in dynamo.getMessages()]
+    return render_template('index.html', messages = messages)
 
 if __name__ == '__main__':
     app.run(debug = True)
